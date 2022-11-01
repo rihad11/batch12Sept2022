f30952f75ce17bafb344194273fe0195ce472c14
Rakibul Hasan <43155356+rihad11@users.noreply.github.com>
2022-10-31 21:00:10 -0400

    added method for dbconnection
    added method for dbconnection

--- src/test/java/com/abc/util/DbConnection.java
+++ src/test/java/com/abc/util/DbConnection.java
@@ -5,26 +5,53 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.testng.annotations.Test;
 
 import com.abc.pages.BaseTest;
 
 public class DbConnection  extends BaseTest{
-
+	static Connection connection;
 	@Test
 	public void dbConnection() throws SQLException {
 		
-		Connection connection= DriverManager.getConnection("jdbc:mysql://localhost/ecommerce",readConfig.getUserSql() ,readConfig.getPassSql());
+		 connection= DriverManager.getConnection("jdbc:mysql://localhost/ecommerce",readConfig.getUserSql() ,readConfig.getPassSql());
+		Log.info("connection eshtablished");
+		List<String> storedataforList 	=DbConnection.getQuery("select * from Customers");
+		
+		for(int i=0; i<storedataforList.size();i++) {
+			String dbdata=storedataforList.get(i);
+			Log.info(dbdata);
+			
+		}
+		
+		
+		
+	}
+	
+	
+	public static List<String> getQuery(String query) throws SQLException {
+		
+		List<String> data= new ArrayList<String>();
+		
 		Statement stmnt = connection.createStatement();
-		String query="select * from Customers where CustomerID=1";
 		ResultSet rs = stmnt.executeQuery(query);
 		
 		while (rs.next()) {
 			
-		//	Log.info(rs.getString("City"));
-			Log.info(rs.getString(2));
-		}
+			
+			data.add(rs.getString(1));
+			data.add(rs.getString(2));
+			
+			
+			}
+		return data;
+		
+		
+		
+		
 		
 	}
 			
